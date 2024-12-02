0c8126434e3ba7ae87a51d0c3d528c371402c213
Your Name <you@example.com>
2024-12-02 21:34:17 +0100

    added a new design

--- service1/src/app.controller.ts
+++ service1/src/app.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get, Post, Render, Body } from '@nestjs/common';
 import { CaptchaService } from './captcha.service';
-
+const sgMail = require('@sendgrid/mail');
 
 @Controller()
 export class AppController {
-  constructor(private readonly captchaService: CaptchaService) {}
+  constructor(private readonly captchaService: CaptchaService) { }
 
   @Get()
   @Render('index') // Render 'index' template
@@ -12,15 +12,33 @@ export class AppController {
     return { title: 'Welcome to NestJS', message: 'This is a simple NestJS app.' };
   }
 
-  getSecret(): string {
-    return process.env.recaptcha_key || 'default';
+  sendMail() {
+    sgMail.setApiKey('YOUR_SENDGRID_API_KEY');
+
+    const msg = {
+      to: 'recipient@example.com', // Recipient's email address
+      from: 'your-email@example.com', // Verified sender email
+      subject: 'Test Email from GCP',
+      text: 'Hello, this is a test email sent from GCP using SendGrid!',
+      html: '<strong>Hello, this is a test email sent from GCP using SendGrid!</strong>',
+    };
+
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent successfully!');
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+      });
+
   }
 
   @Post('contacts') // POST /contacts
   async handleContactForm(@Body() body: any) {
     const token = body['g-recaptcha-response'];
     console.log(token);
-    const { name, email, message} = body;
+    const { name, email, message } = body;
 
     // Verify CAPTCHA
     const isCaptchaValid = await this.captchaService.verifyCaptcha(token);
